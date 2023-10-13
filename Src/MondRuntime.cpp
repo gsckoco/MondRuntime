362270d6e8007aef818c67824aab4d3c38d40971
gsckoco <gsckmp4@outlook.com>
2023-10-13 22:46:46 +0100

    Add project files.

---
+++ Src/MondRuntime.cpp
@@ -0,0 +1,12 @@
+﻿// MondRuntime.cpp : Defines the entry point for the application.
+//
+
+#include "MondRuntime.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
